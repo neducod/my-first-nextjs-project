99fd5a24e96e0af700bf9ee7dae973eef3cd58ce
neducod <nedunony16@gmail.com>
2026-07-28 22:51:27 +0100

    i have an idea and this has to do with routing and creating a component folder, done that already though

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,13 @@
 import Image from "next/image";
 import Hero from "./components/Hero";
 import About from "./components/About";
-import Imageslide from "./components/Imageslide";
 
-import Test from "./components/Test";
 
 export default function Home() {
   return (
     <main className="">
-      {/* <Hero/> */}
-      {/* <About/> */}
-      <Test/>
+      <Hero/> 
+      <About/> 
     </main>
   );
 }
