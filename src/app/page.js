a9979e1b16ababf43cc1644780445f5b838ae549
neducod <nedunony16@gmail.com>
2026-07-29 23:02:39 +0100

    contact file created and imported

--- src/app/page.js
+++ src/app/page.js
@@ -5,6 +5,7 @@ import Campaign from "./components/Campaign"
 import Editorial from "./components/Editorial"
 import Runway from "./components/Runway"
 import Story from "./components/Story"
+import Contact from "./components/Contact"
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Editorial/>
       <Runway/>
       <Story/>
+      <Contact/>
     </main>
   );
 }
