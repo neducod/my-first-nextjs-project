11bbec18f01f24277c463a1999a28db3a87d27dd
neducod <nedunony16@gmail.com>
2026-07-29 06:52:22 +0100

    campaign file imported on the main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar"
 import Collections from "./components/Collections"
+import Campaign from "./components/Campaign";
 
 export default function Home() {
   return (
     <main className="pt-44">
       <Navbar/>
       <Collections/>
+      <Campaign/>
     </main>
   );
 }
